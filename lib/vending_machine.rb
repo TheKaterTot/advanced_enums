6bda0cca2232a75b5e62aafb51b5109ffa934bc7
Katy Farmer <dreadpiratekaty@gmail.com>
2017-01-05 09:51:57 -0700

    Adds quantity

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -10,4 +10,24 @@ attr_reader :inventory
   def add_snack(snack)
     @inventory << snack
   end
+
+  def snacks_by_name
+    @inventory.map do |snack|
+      snack.name
+    end
+  end
+
+  def how_many_snacks
+    @inventory.group_by do |snack|
+      snack.quantity
+    end
+  end
+
+  def inventory_by_alphabet
+    @inventory.group_by do |snack|
+      snack.name[0]
+    end
+  end
+
+
 end
--- test/vending_machine_test.rb
+++ test/vending_machine_test.rb
@@ -43,7 +43,6 @@ class VendingMachineTest < Minitest::Test
   end
 
   def test_can_gather_just_names_of_snacks
-    skip
     vm = VendingMachine.new
     snack1 = Snack.new("White Castle Burger", 1)
     snack2 = Snack.new("Snickers", 4)
@@ -55,7 +54,6 @@ class VendingMachineTest < Minitest::Test
   end
 
   def test_can_collect_items_by_quantity
-    skip
     vm = VendingMachine.new
     snack1 = Snack.new("White Castle Burger", 1)
     snack2 = Snack.new("Snickers", 4)
@@ -73,7 +71,6 @@ class VendingMachineTest < Minitest::Test
   end
 
   def test_can_collect_all_items_by_first_letter_of_alph
-    skip
     vm = VendingMachine.new
     snack1 = Snack.new("White Castle Burger", 1)
     snack2 = Snack.new("Snickers", 4)
@@ -89,7 +86,6 @@ class VendingMachineTest < Minitest::Test
   end
 
   def test_total_number_of_items_in_vending_machine
-    skip
     vm = VendingMachine.new
     snack1 = Snack.new("White Castle Burger", 1)
     snack2 = Snack.new("Snickers", 4)
